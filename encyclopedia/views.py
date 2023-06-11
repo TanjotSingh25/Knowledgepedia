51b5581c82d8532457df7c0e152b578dd5388368
Tanjot Singh <shbhprt@gmail.com>
2023-06-11 11:25:55 -0700

    Basic Frontend Implementation

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
+from django import forms
 
 from . import util
 
+class SearchForm(forms.Form):
+    query = forms.CharField(label="")
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": util.list_entries(),
+        "form": SearchForm()
     })
 
--- wiki/urls.py
+++ wiki/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("encyclopedia.urls"))
+    path('', include("encyclopedia.urls")),
+    path(
+        "favicon.ico",
+        RedirectView.as_view(url="/static/images/favicon.ico"),
+    ),
 ]
